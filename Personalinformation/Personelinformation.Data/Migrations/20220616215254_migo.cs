7dbbe2ec0758e1c340db211ace768eae685c8349
Mahir Aksin <98876295+MahirAKSIN@users.noreply.github.com>
2022-06-17 01:25:15 +0300

    Add Rest.API

---
+++ Personalinformation/Personelinformation.Data/Migrations/20220616215254_migo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Personelinformation.Data.Migrations
+{
+    public partial class migo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
